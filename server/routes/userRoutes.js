60e5fe971b30656a6cf707d18d8c2412bcfd1405
raheem <olanrewajuafolabi873@gmail.com>
2026-07-06 07:23:38 -0700

    Fix remaining model import case issues

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
-import User from '../models/User.js';
+import User from '../models/user.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
