5fa41da8d0fbd4ab80994e00611695f81b1f10a9
raheem <olanrewajuafolabi873@gmail.com>
2026-07-06 10:41:17 -0700

    Fix main render syntax

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { CartProvider } from './context/CartContext.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { CartProvider } from "./context/CartContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <CartProvider>
-    <App/>
-    </CartProvider>,
-
+      <App />
+    </CartProvider>
   </StrictMode>
 );
